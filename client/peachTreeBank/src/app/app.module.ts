65241f9788b7a15d304b7485fa5c20905a8db242
yasmin yazdi <yyazdi13@gmail.com>
2020-10-10 21:00:37 -0500

    modal setup and two-way binding

--- client/peachTreeBank/src/app/app.module.ts
+++ client/peachTreeBank/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TransferBoxComponent } from './transfer-box/transfer-box.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TransferBoxComponent } from './transfer-box/transfer-box.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/peachTreeBank/src/app/transfer-box/transfer-box.component.ts
+++ client/peachTreeBank/src/app/transfer-box/transfer-box.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transfer-box.component.css']
 })
 export class TransferBoxComponent implements OnInit {
-
+  total = <number> 5824.76;
+  public to = '';
+  public amount = '';
   constructor() { }
 
+  visible = <boolean> false;
   ngOnInit(): void {
+  }
+
+  preview(){
+    this.visible = true;
+  }
+
+  exitPreview(){
+    this.visible = false;
   }
 
 }
