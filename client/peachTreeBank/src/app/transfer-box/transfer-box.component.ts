fd2b5e6cd8fecfbd9a2dccc71fd3160126abceef
yasmin yazdi <yyazdi13@gmail.com>
2020-10-12 00:29:21 -0500

    styled icons and search bar area

--- client/peachTreeBank/src/app/transfer-box/transfer-box.component.ts
+++ client/peachTreeBank/src/app/transfer-box/transfer-box.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TransferBoxComponent implements OnInit {
   total = <number> 5824.76;
   public to = '';
-  amount: number;
+  public amount = <any> '';
   constructor() { }
 
   visible = <boolean> false;
